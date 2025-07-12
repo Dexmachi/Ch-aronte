0012083f5cfe08488feb896b267ecde7b74e73cb
Caio Rocha <232001371@aluno.unb.br>
2025-07-12 11:09:02 -0300

    deletando coisa inutil

--- ativar.sh
+++
@@ -1,6 +0,0 @@
-chmod +x sistema/scripts/instalacao.sh
-chmod +x sistema/scripts/reflector.sh
-chmod +x particionamento/scripts/particionamento.sh
-chmod +x chroot/scripts/bootloader.sh
-chmod +x chroot/scripts/personalizacao.sh
-chmod +x chroot/scripts/regiao.sh
