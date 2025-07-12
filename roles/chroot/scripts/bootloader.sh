0b36b4b65f79bd8be190cde344b117fd23f769b8
Caio Rocha <232001371@aluno.unb.br>
2025-07-12 10:40:13 -0300

    separacão de .sh

---
+++ roles/chroot/scripts/bootloader.sh
@@ -0,0 +1,35 @@
+sleep 1
+echo "belezinha belezinha, agora é a parte fudida, tá preparado?"
+echo "vamo configurar teu refind."
+arch-chroot /mnt refind-install
+read -p "me dá um nome daora aí pro teu disco de root" nome_root
+cat <<EOF >> /mnt/boot/refind/refind.conf
+
+# ---------------------------------------------------------
+# Entrada adicionada automaticamente pelo script Ch-aronte
+# ---------------------------------------------------------
+
+menuentry '$nome_pc' {
+    icon /EFI/refind/icons/os_arch.png
+    volume $nome_pc
+    loader /vmlinuz-linux
+    initrd /initramfs-linux.img
+    options root=LABEL=$nome_root rw add_efi_memmap
+    submenuentry 'fallback' {
+        initrd /initramfs-linux-fallback.img
+    }
+    submenuentry 'bootar pro terminal' {
+        add_options 'systemd.unit=multi-user.target'
+    }
+}
+EOF
+
+lsblk
+echo ""
+read -p "qual era tua partição de root mesmo?" root
+read -p "e tu usava btrfs ou nem (y/N)? " btrfs
+if [[ $btrfs == "y" ]]; then
+    btrfs filesystem label /dev/$root "$nome_root"
+else
+    e2label "/dev/$root" "$nome_root"
+fi
