2649b8c5d49f9494218f05e77ec9251ecf76acdd
Caio Rocha <232001371@aluno.unb.br>
2025-07-21 18:13:11 -0300

    conserto de bug macro

--- scripts/F-bootloader.sh
+++ scripts/F-bootloader.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sleep 1
-nome_pc="$(cat /mnt/etc/hostname")
+nome_pc="$(cat /mnt/etc/hostname)"
 echo "belezinha belezinha, agora é a parte fudida, tá preparado?"
 echo "vamo configurar teu refind."
 arch-chroot /mnt refind-install
 read -p "me dá um nome daora aí pro teu disco de root" nome_root
-cat <<EOF >> /mnt/boot/refind/refind.conf
+cat <<EOF >>/mnt/boot/refind/refind.conf
 
 # ---------------------------------------------------------
 # Entrada adicionada automaticamente pelo script Ch-aronte
@@ -31,8 +31,8 @@ echo ""
 read -p "qual era tua partição de root mesmo?" root
 read -p "e tu usava btrfs ou nem (y/N)? " btrfs
 if [[ $btrfs == "y" ]]; then
-    btrfs filesystem label /dev/$root "$nome_root"
+  btrfs filesystem label /dev/$root "$nome_root"
 else
-    e2label "/dev/$root" "$nome_root"
+  e2label "/dev/$root" "$nome_root"
 fi
 arch-chroot /mnt systemctl enable NetworkManager
