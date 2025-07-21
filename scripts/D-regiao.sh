75551a65f9b603e9499ad26889637b2e409e12f9
Caio Rocha <232001371@aluno.unb.br>
2025-07-21 18:15:30 -0300

    conserto de possível globbing ou erro durante read

--- scripts/D-regiao.sh
+++ scripts/D-regiao.sh
@@ -4,13 +4,13 @@ echo "tá, teu sistema tá instalado, agora bora pra parte legal"
 echo "primeiro, vamos usar ln -sf /usr/share/zoneinfo/(sua região) pra consertar esse relógio..."
 sleep 1
 echo "pesquisa aí a região de timedatectl mais próxima de você (ou não escreva nada pra deixar em SP)"
-read -p "Região: " region
+read -p -r "Região: " region
 if [ "$region" == "" ]; then
   region="America/Sao_Paulo"
 fi
 while [ ! -f "/usr/share/zoneinfo/$region" ]; do
   echo "Região inválida! tente de novo "
-  read -p "Região: " region
+  read -p -r "Região: " region
 done
 ln -sf "/usr/share/zoneinfo/$region" "/mnt/etc/localtime"
 
@@ -27,10 +27,10 @@ sleep 1
 nano /mnt/etc/locale.gen
 sleep 1
 arch-chroot /mnt locale-gen
-read -p "agora, me diga a linha que tu descomentou, só coloca a região, tipo 'pt_BR' ou 'en_US' e SIM, preciso das duas letras maíusculas no final. " lingua
+read -p -r "agora, me diga a linha que tu descomentou, só coloca a região, tipo 'pt_BR' ou 'en_US' e SIM, preciso das duas letras maíusculas no final. " lingua
 while ! grep -q "^$lingua.UTF-8" /mnt/etc/locale.gen; do
   echo "Locale '$lingua' não encontrado no locale.gen"
-  read -p "Tenta de novo, com algo tipo pt_BR ou en_US: " lingua
+  read -p -r "Tenta de novo, com algo tipo pt_BR ou en_US: " lingua
 done
 touch /mnt/etc/locale.conf
 echo "LANG=$lingua.UTF-8" >/mnt/etc/locale.conf
--- scripts/E-personalizacao.sh
+++ scripts/E-personalizacao.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-read -p "ok, me dá um nome pro seu pc aí... e pelo amor de deus, me dá um nome bonito" nome_pc
+read -p -r "ok, me dá um nome pro seu pc aí... e pelo amor de deus, me dá um nome bonito" nome_pc
 while [[ -z "$nome_pc" || "$nome_pc" =~ [^a-zA-Z0-9.-] ]]; do
-  read -p "Nome inválido. Só letras, números, ponto ou hífen. Tenta de novo aí oh jegue: " nome_pc
+  read -p -r "Nome inválido. Só letras, números, ponto ou hífen. Tenta de novo aí oh jegue: " nome_pc
 done
 echo "$nome_pc" >/mnt/etc/hostname
 sleep 1
@@ -15,7 +15,7 @@ arch-chroot /mnt passwd
 sleep 1
 echo "agora me fala teu user, vamo criar e tacar ele lá no sudoers"
 sleep 1
-read -p "Nome teu usuário: " nome_user
+read -p -r "Nome teu usuário: " nome_user
 arch-chroot /mnt useradd -m -g users -G wheel -s /bin/bash "$nome_user"
 arch-chroot /mnt passwd "$nome_user"
 if ! grep -q "^%wheel ALL=(ALL:ALL) ALL" /mnt/etc/sudoers; then
