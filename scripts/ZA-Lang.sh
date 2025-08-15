9a14cddd2b16ce9850bee83fd24b32452f84c6ee
Caio Rocha <232001371@aluno.unb.br>
2025-08-15 16:46:53 -0300

    .

--- scripts/ZA-Lang.sh
+++ scripts/ZA-Lang.sh
@@ -1,7 +1,8 @@
 touch respostas.env
 echo "Choose your language / Escolha sua linguagem:"
-read -p "1- English / 2- Português: " -r lang
-while [[ "$lang" != "1" || "$lang" != "2" ]]; do
+
+while true; do
+  read -p "1- English / 2- Português: " -r lang
   if [[ "$lang" == "1" ]]; then
     echo "You chose English"
     LANGC="English"
@@ -12,11 +13,10 @@ while [[ "$lang" != "1" || "$lang" != "2" ]]; do
     break
   else
     echo "Por favor, escolha uma opção válida / Please choose a valid option."
-    read -p "1- English / 2- Português: " -r lang
   fi
 done
 
-echo "$LANGC" >>respostas.env
+echo "LANGC=$LANGC" >respostas.env
 set -a
 source respostas.env
 set +a
