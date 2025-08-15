ab8efec9cc36197879730848d5d0c655e63f02d3
Caio Rocha <232001371@aluno.unb.br>
2025-08-15 16:32:33 -0300

    bug sqash

--- scripts/ZA-Lang.sh
+++ scripts/ZA-Lang.sh
@@ -1,23 +1,17 @@
-echo "chose your language / Escolha sua linguagem:"
-read -p "1- English / 2- Português: " -r lang
-if [[ $lang == 1 ]]; then
-  echo "You chose English"
-  LANG=English
-elif [[ $lang == 2 ]]; then
-  echo "Você escolheu Português"
-  LANG=Portugues
-fi
-
-while [[ -z "$lang" || ! "$lang" =~ ^(1|2)$ ]]; do
-  echo "Por favor, escolha uma opção válida./please choose a valid option."
+echo "Choose your language / Escolha sua linguagem:"
+while true; do
   read -p "1- English / 2- Português: " -r lang
-  if [[ $lang == 1 ]]; then
+  if [[ "$lang" == "1" ]]; then
     echo "You chose English"
-    LANG=English
-  elif [[ $lang == 2 ]]; then
+    LANG="English"
+    break
+  elif [[ "$lang" == "2" ]]; then
     echo "Você escolheu Português"
-    LANG=Portugues
+    LANG="Portugues"
+    break
+  else
+    echo "Por favor, escolha uma opção válida / Please choose a valid option."
   fi
 done
 
-echo "$lang" >>respostas.env
+echo "$LANG" >>respostas.env
