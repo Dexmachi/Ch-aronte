03136314273b903c66ed88079184972e8bda4d84
Caio Rocha <232001371@aluno.unb.br>
2025-10-02 09:07:31 -0300

    feat/starting dotfile_manager

---
+++ scripts/G-dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -a
+source respostas.env
+set +a
+
+case "$LANGC" in
+"Portugues")
+  MSG_LOADING_DOTS="Fecho, carregando suas dots."
+  ;;
+"English")
+  MSG_LOADING_DOTS="Ight, loading ya dots."
+  ;;
+*)
+  echo "LANGUAGE NOT RECOGNIZED, PLEASE USE EITHER ENGLISH OR PORTUGUESE."
+  ;;
+
+esac
+
+echo "$MSG_LOADING_DOTS"
+sleep 0.5
+ansible-playbook -vvv ./main.yaml --tags dotfiles
