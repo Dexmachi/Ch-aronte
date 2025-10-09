85ff571babb97f74f9afcae0c7fdbf758b8835b9
Caio Rocha <232001371@aluno.unb.br>
2025-10-08 21:19:16 -0300

    feat/COLORS!

--- scripts/ZA-Lang.sh
+++ scripts/ZA-Lang.sh
@@ -1,4 +1,24 @@
 touch respostas.env
+echo -en "\e]50694e26" # black (background)
+echo -en "\e]9d41318e" # red
+echo -en "\e]65d0606a" # green
+echo -en "\e]d0cf6068" # yellow
+echo -en "\e]60d0ccf7" # blue
+echo -en "\e]a560d0f7" # magenta
+echo -en "\e]60d0bfff" # cyan
+echo -en "\e]c7f0c9d6" # white
+echo -en "\e]171a1768" # bright black
+echo -en "\e]cc17178e" # bright red
+echo -en "\e]3ccc176a" # bright green
+echo -en "\e]c8d20068" # bright yellow
+echo -en "\e]0048d2f7" # bright blue
+echo -en "\e]8900d2f7" # bright magenta
+echo -en "\e]00d2a6ff" # bright cyan
+echo -en "\e]c4ffbbf5" # bright white (foreground)
+
+# Set default foreground and background
+echo -en "\033[0m"
+
 echo "Choose your language / Escolha sua linguagem:"
 
 while true; do
