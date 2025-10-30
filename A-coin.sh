ce5b97c63c6fe7110e7e738011903b5607c3f1d4
Caio Rocha <232001371@aluno.unb.br>
2025-10-30 08:18:04 -0300

    lint

--- A-coin.sh
+++ A-coin.sh
@@ -4,7 +4,7 @@ set -a
 source ./respostas.env
 set +a
 if [ ! "$LANGC" ]; then
-  source scripts/ZA-Lang.sh
+	source scripts/ZA-Lang.sh
 fi
 
 set -a
@@ -24,12 +24,12 @@ set -a
 source ./respostas.env
 set +a
 if [ "$PARTITIONED" == "true" ]; then
-  read -p "$MSG_PROMPT_RUN_AGAIN_PARTITION" -r choice
-  if [[ "$choice" =~ ^[YySs]$ ]]; then
-    bash scripts/A-particionamento.sh
-  fi
+	read -p "$MSG_PROMPT_RUN_AGAIN_PARTITION" -r choice
+	if [[ "$choice" =~ ^[YySs]$ ]]; then
+		bash scripts/A-particionamento.sh
+	fi
 else
-  bash scripts/A-particionamento.sh
+	bash scripts/A-particionamento.sh
 fi
 
 # --- Reflector ---
@@ -37,12 +37,12 @@ set -a
 source ./respostas.env
 set +a
 if [ "$REFLECTORED" == "true" ]; then
-  read -p "$MSG_PROMPT_RUN_AGAIN_REFLECTOR" -r choice
-  if [[ "$choice" =~ ^[YySs]$ ]]; then
-    bash scripts/B-reflector.sh
-  fi
+	read -p "$MSG_PROMPT_RUN_AGAIN_REFLECTOR" -r choice
+	if [[ "$choice" =~ ^[YySs]$ ]]; then
+		bash scripts/B-reflector.sh
+	fi
 else
-  bash scripts/B-reflector.sh
+	bash scripts/B-reflector.sh
 fi
 
 # --- Instalação Base ---
@@ -50,12 +50,12 @@ set -a
 source ./respostas.env
 set +a
 if [ "$INSTALLED" == "true" ]; then
-  read -p "$MSG_PROMPT_RUN_AGAIN_INSTALL" -r choice
-  if [[ "$choice" =~ ^[YySs]$ ]]; then
-    bash scripts/C-instalacao.sh
-  fi
+	read -p "$MSG_PROMPT_RUN_AGAIN_INSTALL" -r choice
+	if [[ "$choice" =~ ^[YySs]$ ]]; then
+		bash scripts/C-instalacao.sh
+	fi
 else
-  bash scripts/C-instalacao.sh
+	bash scripts/C-instalacao.sh
 fi
 bash scripts/D-regiao.sh
 bash scripts/E-personalizacao.sh
@@ -66,12 +66,12 @@ source ./respostas.env
 set +a
 
 if [ "$FULLCODERAN" == "true" ]; then
-  PLAYBOOK_CHROOT_PATH="/mnt/root/Ch-aronte/main.yaml"
-  PLAYBOOK_CH_OBOLOS_PATH="/mnt/root/Ch-aronte/Ch-obolos/$PLUGIN"
-  cp -r ./ /mnt/root/Ch-aronte/
-  arch-chroot /mnt ansible-playbook $PLAYBOOK_CHROOT_PATH --tags "$CHROOT_TAGS",region,users,bootloader,services -e @"$PLAYBOOK_CH_OBOLOS_PATH"
-  if [ "$DOTS_ACCEPT" == "yes" ]; then
-    echo "$MSG_LOADING_DOTS"
-    arch-chroot /mnt ansible-playbook $PLAYBOOK_CHROOT_PATH --tags dotfiles -e @"$PLAYBOOK_CH_OBOLOS_PATH"
-  fi
+	PLAYBOOK_CHROOT_PATH="/mnt/root/Ch-aronte/main.yaml"
+	PLAYBOOK_CH_OBOLOS_PATH="/mnt/root/Ch-aronte/Ch-obolos/$PLUGIN"
+	cp -r ./ /mnt/root/Ch-aronte/
+	arch-chroot /mnt ansible-playbook $PLAYBOOK_CHROOT_PATH --tags "$CHROOT_TAGS",region,users,bootloader,services -e @"$PLAYBOOK_CH_OBOLOS_PATH"
+	if [ "$DOTS_ACCEPT" == "yes" ]; then
+		echo "$MSG_LOADING_DOTS"
+		arch-chroot /mnt ansible-playbook $PLAYBOOK_CHROOT_PATH --tags dotfiles -e @"$PLAYBOOK_CH_OBOLOS_PATH"
+	fi
 fi
